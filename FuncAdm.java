97e76bb458cae72a2dcb983bbed7dc82647c3139
spornraftjulia <118003937+spornraftjulia@users.noreply.github.com>
2023-05-09 11:00:40 -0300

    FuncAdm.java

--- FuncAdm.java
+++ FuncAdm.java
@@ -1,10 +1,26 @@
 public class FuncAdm extends Funcionario{
 
-    public FuncAdm(String nome, int idade, String endereco){
-        super(nome, idade, endereco);
-    }
-    public FuncAdm(){
-        
+    public void setSetor(String setor) {
+        this.setor = setor;
     }
     
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
+    public String getSetor(){
+        return setor;
+    }
+    
+    public String getFuncao(){
+        return funcao;
+    }
+    
+    public FuncAdm(String nome, int idade, String endereco, double salario, String setor, String funcao){
+        super(nome, idade, endereco, salario);
+    
+        this.setor = setor;
+        this.funcao = funcao;
+    }
+    public FuncAdm();
 }
